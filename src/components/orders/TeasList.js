7f60f46e80c18a8f167f401531e00763217de30d
seintun <seintun.edu@gmail.com>
2018-11-28 02:05:47 -0800

    Added dropdown selection component for teas and toppings

--- src/components/orders/TeasList.js
+++ src/components/orders/TeasList.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import Teas from './Teas'
+import { Dropdown } from 'semantic-ui-react'
 
-const TeasList = () => {
-  return (
-    <div>
-      <Teas />
-    </div>
-  )
-}
+const teaOptions = [
+  {
+    text: 'Anita’s Blend (Black Tea)',
+    value: 'Anita’s Blend',
+    image: { avatar: true, src: 'https://st2.depositphotos.com/9223672/12056/v/950/depositphotos_120568134-stock-illustration-male-face-avatar-logo-template.jpg' },
+  },
+  {
+    text: 'Irish Breakfast (Black Tea)',
+    value: 'Irish Breakfast',
+    image: { avatar: true, src: 'https://st2.depositphotos.com/9223672/12056/v/950/depositphotos_120568134-stock-illustration-male-face-avatar-logo-template.jpg' },
+  },
+  {
+    text: 'Meyer Berry Sencha (Green Tea)',
+    value: 'Meyer Berry Sencha',
+    image: { avatar: true, src: 'https://st2.depositphotos.com/9223672/12056/v/950/depositphotos_120568134-stock-illustration-male-face-avatar-logo-template.jpg' },
+  }
+]
+const TeasList = () => (
+  <Dropdown placeholder='Select the tea type' fluid selection options={teaOptions} />
+)
 
 export default TeasList
--- src/components/orders/ToppingsList.js
+++ src/components/orders/ToppingsList.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import Toppings from './Toppings'
+import { Dropdown } from 'semantic-ui-react'
 
-const ToppingsList = () => {
-  return (
-    <div>
-      <Toppings />
-    </div>
-  )
-}
+const toppingOptions = [
+  { key: 'Boba', text: 'Boba', value: 'Boba' },
+  { key: 'pudding', text: 'Pudding', value: 'pudding' },
+  { key: 'natajelly', text: 'Nata Jelly', value: 'natajelly' },
+  { key: 'mungbean', text: 'Mung Bean', value: 'mungbean' },
+  { key: 'herbaljelly', text: 'Herbal Jelly', value: 'herbaljelly' },
+  { key: 'grapepb', text: 'Grape Popping Bubbles', value: 'grapepb' },
+  { key: 'mangopb', text: 'Mango Popping Bubbles', value: 'mangopb' },
+  { key: 'coffeepb', text: 'Coffee Popping Bubbles', value: 'coffeepb' },
+  { key: 'aloejelly', text: 'Aloe Jelly', value: 'aloejelly' },
+  { key: 'figjelly', text: 'Fig Jelly', value: 'figjelly' },
+]
+
+const ToppingsList = () => (
+  <Dropdown placeholder='Select any Toppings' fluid multiple selection options={toppingOptions} />
+)
 
 export default ToppingsList
