ab8d43985c5b1103b1f3dd550ce4b7fb8d937b46
seintun <seintun.edu@gmail.com>
2018-11-27 20:55:02 -0800

    added progress to track the steps of ordering process

---
+++ src/components/orders/OrderDashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import OrderProgressBar from '../orders/OrderProgressBar'
+
+const ContainerExampleContainer = () => (
+  <div>
+    <OrderProgressBar />
+  </div>
+)
+
+export default ContainerExampleContainer
