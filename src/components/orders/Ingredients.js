666319880225367f1525fcfa88d49ab74214f6b4
seintun <seintun.edu@gmail.com>
2018-11-28 16:53:30 -0800

    Added interactive buttons for orders ingredient customization

---
+++ src/components/orders/Ingredients.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import { Label, Button, Segment } from 'semantic-ui-react'
+
+export default class Ingredients extends Component {
+  state= {
+    milk: false,
+    sugar: 1.00,
+    ice: 1.00,
+    milkStatus: 'Lactose-free milk included'
+  }
+  // Toggle status of milk
+  handleMilkClick = () => this.state.milk === false ? this.setState({ milk: true, milkStatus: 'Do NOT add any milk' }) : this.setState({ milk: false, milkStatus: 'Lactose-free milk included' })
+  // Plus or Minus sugar by increments of 0.25
+  handlePlusSugarClick = () => this.state.sugar !== 2.00 ? this.setState({ sugar: (this.state.sugar + 0.25) }) : this.setState({ sugar: 2.00 })
+  handleMinusSugarClick = () => this.state.sugar !== 0.00 ? this.setState({ sugar: (this.state.sugar - 0.25) }) : this.setState({ sugar: 0.00 })
+
+  // Plus or Minus ice by increments of 0.25
+  handlePlusIceClick = () => this.state.ice !== 2.00 ? this.setState({ ice: (this.state.ice + 0.25) }) : this.setState({ ice: 2.00 })
+  handleMinusIceClick = () => this.state.ice !== 0.00 ? this.setState({ ice: (this.state.ice - 0.25) }) : this.setState({ ice: 0.00 })
+
+  render() {
+    return (
+      <div>
+        <Segment>
+          <Button.Group>
+            <Button animated primary onClick={ this.handleMilkClick }>
+              <Button.Content visible>Click to <br/> Add or Remove Milk</Button.Content>
+              <Button.Content hidden>(Lactose-free Milk)</Button.Content>
+            </Button>
+            <Label color='purple'>
+              <h3>{ this.state.milkStatus }</h3>
+            </Label>
+          </Button.Group>
+
+        </Segment>
+        <Segment>
+          <Button.Group>
+            <Button onClick={ this.handleMinusSugarClick }>Less</Button>
+            <Button.Or />
+            <Button positive onClick={ this.handlePlusSugarClick }>More</Button>
+            <Label image>
+              <img src='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
+              Sugar Level: { this.state.sugar * 100 }%
+            </Label>
+          </Button.Group>
+        </Segment>
+        <Segment>
+          <Button.Group>
+            <Button onClick={this.handleMinusIceClick}>Less</Button>
+            <Button.Or />
+            <Button positive onClick={this.handlePlusIceClick}>More</Button>
+            <Label image>
+              <img src='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
+              Ice Level: {this.state.ice * 100}%
+            </Label>
+          </Button.Group>
+        </Segment>
+      </div>
+    )
+  }
+}
--- src/components/orders/OrderDashboard.js
+++ src/components/orders/OrderDashboard.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import { Segment } from 'semantic-ui-react'
 import OrderProgressBar from '../orders/OrderProgressBar'
 import TeasList from './TeasList'
 import ToppingsList from './ToppingsList'
+import Ingredients from './Ingredients'
 
 const ContainerExampleContainer = () => (
   <div>
-    <OrderProgressBar />
-    <h4>Tea Selection:</h4>
-    <TeasList />
-    <h4>Toppings Selection:</h4>
-    <ToppingsList />
+    <Segment.Group>
+      <OrderProgressBar />
+      <Segment>
+        <h4>Tea Selection <em>(Pick one)</em>:</h4>
+        <TeasList />
+      </Segment>
+      <Segment>
+        <h4>Topping Selection <em>(Pick one or more)</em>:</h4>
+        <ToppingsList />
+      </Segment>
+      <Segment>
+        <Ingredients />
+      </Segment>
+    </Segment.Group>
+    
   </div>
 )
 
