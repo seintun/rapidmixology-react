e83b056875e092cabfd70cfe82c7173c821d03b7
seintun <seintun.edu@gmail.com>
2018-11-28 20:42:26 -0800

    Added components for Registration & Login Page with form

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react'
 import NavMenu from '../common/NavMenu'
 import OrderFlow from '../orders/OrderFlow'
 
+import Register from '../auth/Register'
+import Login from '../auth/Login'
 export default class Dashboard extends Component {
+  state= {
+    registerDisplay: false,
+    loginDisplay: false,
+    orderFlowDisplay: true
+  }
   render() {
     return (
       <div>
-        <NavMenu />
-        <OrderFlow />
+        <NavMenu 
+          dashboardDisplay = { this.state }
+        />
+        { this.state.orderFlowDisplay ? <OrderFlow /> : false }
+        { this.state.registerDisplay ? <Register /> : false }
+        { this.state.loginDisplay ?  <Login /> : false }
       </div>
     )
   }
