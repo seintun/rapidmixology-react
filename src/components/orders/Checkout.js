ebaf02dbe884bfdc72b39b9c918c35a16915f73e
seintun <seintun.edu@gmail.com>
2018-12-08 13:19:44 -0800

    Created CheckoutItems components to render order details

--- src/components/orders/Checkout.js
+++ src/components/orders/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import LoggedInCheckoutAlert from '../common/alerts/LoggedInCheckout.alert'
 import LoggedOutCheckoutAlert from '../common/alerts/LoggedOutCheckout.alert'
+import CartItems from './CheckoutItems'
+import { Segment, Button, Icon } from 'semantic-ui-react';
 export default class Checkout extends Component {
   state= {}
   render() {
@@ -8,6 +10,17 @@ export default class Checkout extends Component {
     return (
       <div>
         { token ? <LoggedInCheckoutAlert /> : <LoggedOutCheckoutAlert /> }
+        <Segment>
+          <Segment attached>
+            <CartItems orders={ this.props.orders }/>
+          </Segment>
+          <Button attached='bottom' animated positive>
+            <Button.Content visible>Place Order</Button.Content>
+            <Button.Content hidden>
+              <Icon name='shop' />
+            </Button.Content>
+          </Button>
+        </Segment>
       </div>
     )
   }
